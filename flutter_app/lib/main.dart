a06f7c10b2dd799634588374c89d4614b4430e52
moniiica0125 <monicapwar1@gmail.com>
2026-03-27 04:02:58 -0700

    Add Flutter main file.

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CodeQuest',
+      home: Scaffold(
+        appBar: AppBar(title: Text('CodeQuest Flutter App')),
+        body: Center(child: Text('Welcome to CodeQuest!')),
+      ),
+    );
+  }
+}
